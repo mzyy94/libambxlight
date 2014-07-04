f5757424074e13ef808278732e1b3cf1ccf1a712
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-07-04 23:59:02 +0900

    support 2 devices

--- driver/test/ambxtest.c
+++ driver/test/ambxtest.c
@@ -11,13 +11,14 @@ int main(int argc, char const* argv[])
 	Window root;
 	unsigned long color;
 	time_t t;
-	FILE *fp;
+	FILE *fp0, *fp1;
 	XImage *ximage;
 	XWindowAttributes attributes;
 
 	dpy = XOpenDisplay(":0");
 	root = DefaultRootWindow(dpy);
-	fp = fopen("/dev/ambx_light0", "w");
+	fp0 = fopen("/dev/ambx_light0", "w");
+	fp1 = fopen("/dev/ambx_light1", "w");
 	XGetWindowAttributes(dpy, root, &attributes);
 
 	unsigned long long int r;
@@ -45,12 +46,30 @@ int main(int argc, char const* argv[])
 		r /= count;
 		g /= count;
 		b /= count;
-		fprintf(fp, "%c%c%c%c%c%c%c%c%c",
+		fprintf(fp0, "%c%c%c%c%c%c%c%c%c",
 				0xa2, 0x00, r, g, b, 0x00, 0x00, 0x00, 0x00);
-		fflush(fp);
+		fflush(fp0);
+		count = 0;
+		r = g = b = 0;
+		for (i = attributes.width / 2; i < attributes.width; i += step) {
+			for (j = 0; j < attributes.height; j += step) {
+				color = XGetPixel(ximage, i, j);
+				r += (color >> 16) & 0xff;
+				g += (color >> 8) & 0xff;
+				b += color & 0xff;
+				count++;
+			}
+		}
+		r /= count;
+		g /= count;
+		b /= count;
+		fprintf(fp1, "%c%c%c%c%c%c%c%c%c",
+				0xa2, 0x00, r, g, b, 0x00, 0x00, 0x00, 0x00);
+		fflush(fp1);
 		XDestroyImage(ximage);
 		usleep(1000000/60);
 	}
-	fclose(fp);
+	fclose(fp0);
+	fclose(fp1);
 	return 0;
 }
