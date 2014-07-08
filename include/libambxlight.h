7903282500b6d86e4f2a7b06bd2cc3828c4682de
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-07-08 16:47:15 +0900

    libambxlight: update library using struct ambxlight_device

--- include/libambxlight.h
+++ include/libambxlight.h
@@ -1,25 +1,29 @@
 #ifndef _LIBAMBXLIGHT_H__
 #define _LIBAMBXLIGHT_H__
 
-#include "../include/ambxlight_params.h"
+#include <stddef.h>
+#include <ambxlight_device.h>
+
+#define MODE_CHANGE_REQUIRED 0x04
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int ambxlight_device_open(int index);
-int ambxlight_device_open_all(int *file_discriptors, int max_devices);
-int ambxlight_device_close(int file_discriptor);
-int ambxlight_device_close_all(int *file_discriptors, int devices);
-int ambxlight_start_color_mode(int file_discriptor);
-int ambxlight_end_color_mode(int file_discriptor);
-int ambxlight_write_color_boost(int file_discriptor, unsigned char* color);
-int ambxlight_change_color_with_fade(int file_discriptor, unsigned char* color, unsigned int speed);
-int ambxlight_set_state(int file_discriptor, unsigned char state);
-int ambxlight_set_intensity(int file_discriptor, unsigned char intensity);
-int ambxlight_set_height(int file_discriptor, unsigned char height);
-int ambxlight_set_location(int file_discriptor, unsigned char location);
-int ambxlight_get_params(int file_discriptor, union ambxlight_params* params);
+int ambxlight_set_color_mode(struct ambxlight_device *device);
+int ambxlight_set_raw_mode(struct ambxlight_device *device);
+int ambxlight_set_hexstring_mode(struct ambxlight_device *device);
+int ambxlight_change_color_boost(struct ambxlight_device device, unsigned char *color);
+int ambxlight_change_color_with_fade(struct ambxlight_device device, unsigned char *color, unsigned int speed);
+int ambxlight_set_state(struct ambxlight_device *device, unsigned char state);
+int ambxlight_set_intensity(struct ambxlight_device *device, unsigned char intensity);
+int ambxlight_set_height(struct ambxlight_device *device, unsigned char height);
+int ambxlight_set_location(struct ambxlight_device *device, unsigned char location);
+int ambxlight_get_params(struct ambxlight_device *device);
+struct ambxlight_device ambxlight_device_open(int index);
+size_t ambxlight_device_open_all(struct ambxlight_device *devices, size_t max_size);
+int ambxlight_device_close(struct ambxlight_device device);
+int ambxlight_device_close_all(struct ambxlight_device *devices, size_t size);
 
 #ifdef __cplusplus
 };
--- lib/libambxlight.c
+++ lib/libambxlight.c
@@ -3,83 +3,51 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 
 #include "../include/ambxlight_ioctl.h"
 #include "../include/ambxlight_params.h"
+#include "../include/ambxlight_device.h"
 
-int __check_device_access(const char* name) {
-	struct stat file_stat;
-	if (stat(name, &file_stat) != 0) {
-		return -1;
-	}
-	if (!S_ISCHR(file_stat.st_mode)) {
-		return -2;
-	}
-	if ( !(((file_stat.st_mode & S_IRWXU) & S_IWUSR) &&
-				((file_stat.st_mode & S_IRWXU ) & S_IRUSR))) {
-		return -4;
-	}
-	return 0;
+#define MODE_CHANGE_REQUIRED 0x04
+#define CHECK_AMBXLIGHT_MODE_COLOR(mode) do { \
+	if (mode != AMBXLIGHT_MODE_COLOR) \
+		return -MODE_CHANGE_REQUIRED; \
+} while (0)
+
+#define CHECK_AMBXLIGHT_MODE_RAW(mode) do { \
+	if (mode != AMBXLIGHT_MODE_RAW) \
+		return -MODE_CHANGE_REQUIRED; \
+} while (0)
+
+#define CHECK_AMBXLIGHT_MODE_HEXSTRING(mode) do { \
+	if (mode != AMBXLIGHT_MODE_HEXSTRING) \
+		return -MODE_CHANGE_REQUIRED; \
+} while (0)
+
+
+int ambxlight_set_color_mode(struct ambxlight_device *device) {
+	device->mode = AMBXLIGHT_MODE_COLOR;
+	return ioctl(device->fd, AMBXLIGHT_IOCTL_SET, &device->mode);
 }
 
-int ambxlight_device_open(int index) {
-	char name[20];
-	int retval;
-	int fd;
-	sprintf(name, "/dev/ambx_light%d", index);
-	if ((retval = __check_device_access(name)) < 0) {
-		return retval;
-	}
-	fd = open(name, O_RDWR);
-	if (fd > 1) {
-		unsigned char mode = AMBXLIGHT_MODE_RAW;
-		ioctl(fd, AMBXLIGHT_IOCTL_SET, &mode);
-	}
-	return fd;
+int ambxlight_set_raw_mode(struct ambxlight_device *device) {
+	device->mode = AMBXLIGHT_MODE_RAW;
+	return ioctl(device->fd, AMBXLIGHT_IOCTL_SET, &device->mode);
 }
 
-int ambxlight_device_open_all(int *file_discriptors, int max_devices) {
-	unsigned int i;
-	unsigned int index = 0;
-	int fd;
-	for (i = 0; i < max_devices; i++) {
-		fd = ambxlight_device_open(i);
-		if (fd > 0) {
-			file_discriptors[index++] = fd;
-		}
-	}
-	return index;
+int ambxlight_set_hexstring_mode(struct ambxlight_device *device) {
+	device->mode = AMBXLIGHT_MODE_HEXSTRING;
+	return ioctl(device->fd, AMBXLIGHT_IOCTL_SET, &device->mode);
 }
 
-int ambxlight_device_close_all(int *file_discriptors, int devices) {
-	unsigned int i;
-	for (i = 0; i < devices; i++) {
-		close(file_discriptors[i]);
-	}
-	return 0;
+int ambxlight_change_color_boost(struct ambxlight_device device, unsigned char *color) {
+	CHECK_AMBXLIGHT_MODE_COLOR(device.mode);
+	return write(device.fd, color, 3);
 }
 
-int ambxlight_device_close(int file_discriptor) {
-	unsigned char mode = AMBXLIGHT_MODE_HEXSTRING;
-	ioctl(file_discriptor, AMBXLIGHT_IOCTL_SET, &mode);
-	return close(file_discriptor);
-}
-
-int ambxlight_start_color_mode(int file_discriptor) {
-	unsigned char mode = AMBXLIGHT_MODE_COLOR;
-	return ioctl(file_discriptor, AMBXLIGHT_IOCTL_SET, &mode);
-}
-
-int ambxlight_end_color_mode(int file_discriptor) {
-	unsigned char mode = AMBXLIGHT_MODE_RAW;
-	return ioctl(file_discriptor, AMBXLIGHT_IOCTL_SET, &mode);
-}
-
-int ambxlight_write_color_boost(int file_discriptor, unsigned char *color) {
-	return write(file_discriptor, color, 3);
-}
-
-int ambxlight_change_color_with_fade(int file_discriptor, unsigned char* color, unsigned int speed) {
+int ambxlight_change_color_with_fade(struct ambxlight_device device, unsigned char *color, unsigned int speed) {
+	CHECK_AMBXLIGHT_MODE_RAW(device.mode);
 	unsigned char data[9] = {
 		0xa2,
 		0x00,
@@ -91,46 +59,112 @@ int ambxlight_change_color_with_fade(int file_discriptor, unsigned char* color, 
 		0x00,
 		0x00
 	};
-	return write(file_discriptor, data, sizeof(data));
+	return write(device.fd, data, sizeof(data));
 }
 
-int ambxlight_set_state(int file_discriptor, unsigned char state) {
+int ambxlight_set_state(struct ambxlight_device *device, unsigned char state) {
+	CHECK_AMBXLIGHT_MODE_RAW(device->mode);
 	unsigned char data[3] = {
 		0xa1,
 		0x00,
 		state
 	};
-	return write(file_discriptor, data, sizeof(data));
+	device->params.param.enabled = state;
+	return write(device->fd, data, sizeof(data));
 }
 
-int ambxlight_set_intensity(int file_discriptor, unsigned char intensity) {
+int ambxlight_set_intensity(struct ambxlight_device *device, unsigned char intensity) {
+	CHECK_AMBXLIGHT_MODE_RAW(device->mode);
 	unsigned char data[3] = {
 		0xa6,
 		0x00,
 		intensity
 	};
-	return write(file_discriptor, data, sizeof(data));
+	device->params.param.intensity = intensity;
+	return write(device->fd, data, sizeof(data));
 }
 
-int ambxlight_set_height(int file_discriptor, unsigned char height) {
+int ambxlight_set_height(struct ambxlight_device *device, unsigned char height) {
+	CHECK_AMBXLIGHT_MODE_RAW(device->mode);
 	unsigned char data[3] = {
 		0xa5,
 		0x00,
 		height
 	};
-	return write(file_discriptor, data, sizeof(data));
+	device->params.param.height = height;
+	return write(device->fd, data, sizeof(data));
 }
 
-int ambxlight_set_location(int file_discriptor, unsigned char location) {
+int ambxlight_set_location(struct ambxlight_device *device, unsigned char location) {
+	CHECK_AMBXLIGHT_MODE_RAW(device->mode);
 	unsigned char data[4] = {
 		0xa4,
 		0x00,
 		location,
 		location ? 0x00 : 0x01
 	};
-	return write(file_discriptor, data, sizeof(data));
+	device->params.param.location = location;
+	device->params.param.center = location ? 0x00 : 0x01;
+	return write(device->fd, data, sizeof(data));
 }
 
-int ambxlight_get_params(int file_discriptor, union ambxlight_params* params) {
-	return read(file_discriptor, params, sizeof(params));
+int ambxlight_get_params(struct ambxlight_device *device) {
+	return read(device->fd, &device->params, sizeof(device->params));
+}
+
+struct ambxlight_device ambxlight_device_open(int index) {
+	char name[20];
+	int retval;
+	struct stat file_stat;
+	struct ambxlight_device device;
+	sprintf(name, "/dev/ambx_light%d", index);
+	if (stat(name, &file_stat) != 0) {
+		return (struct ambxlight_device){-1};
+	}
+	if (!S_ISCHR(file_stat.st_mode)) {
+		return (struct ambxlight_device){-2};
+	}
+	if ( !(((file_stat.st_mode & S_IRWXU) & S_IWUSR) &&
+				((file_stat.st_mode & S_IRWXU ) & S_IRUSR))) {
+		return (struct ambxlight_device){-4};
+	}
+	device.fd = open(name, O_RDWR);
+	if (device.fd > 1) {
+		device.mode = AMBXLIGHT_MODE_RAW;
+		ioctl(device.fd, AMBXLIGHT_IOCTL_SET, &device.mode);
+		ambxlight_get_params(&device);
+	}
+	return device;
+}
+
+size_t ambxlight_device_open_all(struct ambxlight_device *devices, size_t max_size) {
+	unsigned int i;
+	unsigned int size = 0;
+	for (i = 0; i < max_size; i++) {
+		struct ambxlight_device device = ambxlight_device_open(i);
+		if (device.fd > 0) {
+			size++;
+			if (devices == NULL) {
+				devices = (struct ambxlight_device *)malloc(sizeof(struct ambxlight_device));
+			} else {
+				devices = (struct ambxlight_device *)realloc(devices, sizeof(struct ambxlight_device) * size);
+			}
+			devices[size - 1] = device;
+		}
+	}
+	return size;
+}
+
+int ambxlight_device_close(struct ambxlight_device device) {
+	device.mode = AMBXLIGHT_MODE_HEXSTRING;
+	ioctl(device.fd, AMBXLIGHT_IOCTL_SET, &device.mode);
+	return close(device.fd);
+}
+
+int ambxlight_device_close_all(struct ambxlight_device *devices, size_t size) {
+	unsigned int i;
+	for (i = 0; i < size; i++) {
+		ambxlight_device_close(devices[i]);
+	}
+	return 0;
 }
