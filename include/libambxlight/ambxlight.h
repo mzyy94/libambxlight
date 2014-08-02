5f30db55cb6d3f1bfe9976b8b9f866fab4a7558a
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-08-03 05:24:12 +0900

    ambxlight*.h: delete

--- include/libambxlight/ambxlight.h
+++
@@ -1,9 +0,0 @@
-#ifndef _AMBXLIGHT_H__
-#define _AMBXLIGHT_H__
-
-#include <libambxlight/libambxlight.h>
-#include <libambxlight/ambxlight_params.h>
-#include <libambxlight/ambxlight_ioctl.h>
-#include <libambxlight/ambxlight_device.h>
-
-#endif
